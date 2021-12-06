f047b3c3e6487e45ac1aaf071e81313e688676a7
niemczus <87914966+niemczus@users.noreply.github.com>
2021-12-06 13:45:56 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Kamil on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
