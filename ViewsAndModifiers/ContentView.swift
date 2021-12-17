5ecac887bcde13439cb278a5b165fea462cb08fb
niemczus <87914966+niemczus@users.noreply.github.com>
2021-12-17 14:38:10 +0100

    View composition

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,12 +7,28 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.yellow)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
 struct ContentView: View {
     
     @ViewBuilder var spells: some View {
-        Text("Lumos")
-        Text("Obliviate")
-        Text("Alohomora")
+        VStack(spacing: 10) {
+            
+            CapsuleText(text: "First")
+            
+            CapsuleText(text: "Second")
+        }
     }
     
     var body: some View {
